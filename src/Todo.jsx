8fc887c57daac8a8775c425f91052ce293b1ef53
Nurjahon <166979887+n20090920@users.noreply.github.com>
2024-08-02 12:07:25 +0500

    table chiqdi

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -4,45 +4,50 @@ import { useEffect } from "react";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Table } from "react-bootstrap";
+import Dark from "./components/Dark";
 
 const Todo = () => {
   const dispatch = useDispatch();
   const { isLoading, data, error } = useSelector((state) => state.todo);
   useEffect(() => {
     dispatch(fetchTodo());
+    alert("Loading qo'shilgan");
   }, [dispatch]);
 
   console.log(data);
   return (
     <div>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Title</th>
-            <th>Completed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {isLoading ? (
-            <div className="loader"></div>
-          ) : error ? (
-            <h1>Error: {error}</h1>
-          ) : (
-            data.map((todo) => {
-              return (
-                <>
-                  <tr key={todo.id}>
-                    <td>{todo.id}</td>
-                    <td>{todo.title}</td>
-                    <td>{todo.completed ? "✅" : "❌"}</td>
-                  </tr>
-                </>
-              );
-            })
-          )}
-        </tbody>
-      </Table>
+      <Dark />
+      <div className="enclosingTable">
+        <Table striped bordered hover className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Completed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <div className="loader"></div>
+            ) : error ? (
+              <h1>Error: {error}</h1>
+            ) : (
+              data.map((todo) => {
+                return (
+                  <>
+                    <tr key={todo.id}>
+                      <td>{todo.id}</td>
+                      <td>{todo.title}</td>
+                      <td>{todo.completed ? "✅" : "❌"}</td>
+                    </tr>
+                  </>
+                );
+              })
+            )}
+          </tbody>
+        </Table>
+      </div>
     </div>
   );
 };
--- src/components/Dark.jsx
+++ src/components/Dark.jsx
@@ -23,14 +23,14 @@ const Dark = () => {
         style={{
           border: "none",
           backgroundColor: "inherit",
-          marginLeft: "20px",
+          // marginLeft: "20px",
         }}
         onClick={toggleDarkMode}>
         {darkMode ? (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="32"
+            height="32"
             viewBox="0 0 24 24"
             className="img"
             style={{ fill: "white", transform: "rotate(180deg)" }}>
@@ -39,10 +39,10 @@ const Dark = () => {
         ) : (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="32"
+            height="32"
             viewBox="0 0 24 24"
-            className="img2"
+            // className="img2"
             style={{ fill: "rgba(0, 0, 0, 1)" }}>
             <path d="M12 11.807A9.002 9.002 0 0 1 10.049 2a9.942 9.942 0 0 0-5.12 2.735c-3.905 3.905-3.905 10.237 0 14.142 3.906 3.906 10.237 3.905 14.143 0a9.946 9.946 0 0 0 2.735-5.119A9.003 9.003 0 0 1 12 11.807z"></path>
           </svg>
